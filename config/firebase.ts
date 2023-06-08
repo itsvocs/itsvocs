6aedcf8871abdbb52c897c7c294b6c4a3204dae5
Vocs Pouani <jordanpouani@icloud.com>
2023-06-08 23:49:37 +0200

    firebase env

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,13 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjnclpr3eEWPg5sI5VJ-tkdvcJr4SOzcg",
-  authDomain: "vocsblog.firebaseapp.com",
-  projectId: "vocsblog",
-  storageBucket: "vocsblog.appspot.com",
-  messagingSenderId: "726491324151",
-  appId: "1:726491324151:web:dec05993212781d7ccfdf2",
-  measurementId: "G-CSKBGPB433",
+  apiKey:
+    process.env.FIREBASE_API_KEY || "AIzaSyBjnclpr3eEWPg5sI5VJ-tkdvcJr4SOzcg",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "vocsblog.firebaseapp.com",
+  projectId: process.env.FIREBASE_PROJECT_ID || "vocsblog",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "vocsblog.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "726491324151",
+  appId:
+    process.env.FIREBASE_APP_ID || "1:726491324151:web:dec05993212781d7ccfdf2",
+  measurementId: process.env.MEASUREMENT_ID || "G-CSKBGPB433",
 };
 
 // Initialize Firebase
